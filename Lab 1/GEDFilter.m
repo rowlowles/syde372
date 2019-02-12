310c562d41c594c6079b5ba35fff0c8b2a5370f7
Kaley Bibic <kmbibic@gmail.com>
2019-02-11 22:46:31 -0500

    Make GED actually work

--- Lab 1/GEDFilter.m
+++ Lab 1/GEDFilter.m
@@ -20,8 +20,8 @@ end
 minValue = floor(min(mins)) - 5;
 maxValue = ceil(max(maxs)) + 5;
 
-feature1Vals = minValue(1):0.5:maxValue(1); % step was 0.05
-feature2Vals = minValue(2):0.5:maxValue(2);
+feature1Vals = minValue(1):0.05:maxValue(1); % step was 0.05
+feature2Vals = minValue(2):0.05:maxValue(2);
 
 [X, Y] = meshgrid(feature1Vals, feature2Vals);
 
@@ -34,16 +34,11 @@ for i = 1:size(feature1Vals,2)
     for j = 1:size(feature2Vals,2)
         for class = 1:numClasses
             % Extract class info from varargs
-            classData = varargin {(class-1)*numFieldsPerClass + 1};
             classMean = varargin {(class-1)*numFieldsPerClass + 2};
             classCov = varargin {(class-1)*numFieldsPerClass + 3};
             
-            % Iterate through every datapoint in the class
-            for idx = 1:size(classData, 1)
-                point = classData(idx:(idx+1)).';
-                dist = sqrt((point-classMean).'*inv(classCov)*(point-classMean)); % I think this has to include distances to all the class means, not just one (which is what I think it's doing rn)
-                classDist(class,j,i) = dist;
-            end
+            point = [feature1Vals(1,i) ;feature2Vals(1,j)];
+            classDist(class,j,i) = sqrt(sum((point-classMean).'*inv(classCov)*(point-classMean)));
         end
     end
 end
@@ -56,10 +51,8 @@ end
 % Plot the data
 figure
 
-imagesc([minValue(1) maxValue(1)], [minValue(2) maxValue(2)], classifier);
-colormap([0.945 0.835 0.847; 0.835 0.874 0.945; 0.839 0.945 0.835])
+contour(X,Y,classifier);
 hold on;
-set(gca, 'ydir', 'normal');
 classDataAndNameIdxs = zeros(1, numClasses*2);
 idx = 1;
 for classIdx = 1:numFieldsPerClass:size(varargin,2)
@@ -69,5 +62,4 @@ for classIdx = 1:numFieldsPerClass:size(varargin,2)
 end
 plotArgs=varargin(classDataAndNameIdxs);
 plotClasses(plotArgs{:});
-
 end

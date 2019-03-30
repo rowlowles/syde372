cc5c097fb2a345599df097d68fefc95526181271
Laura Bingeman <lbingeman@v1040-wn-rt-b-32-206.campus-dynamic.uwaterloo.ca>
2019-03-30 13:52:50 -0400

    Varience fix

--- Lab 2/lab2.m
+++ Lab 2/lab2.m
@@ -3,7 +3,7 @@ load('lab2_1');
 load('lab2_2');
 
 % Parametric Estination - 1D
-parametricEstimator1D(a);
+parametricEstimator1D(a(1:4));
 
 % Parametric Estimation - 2D
 parametricEstimation2D(al,bl,cl);
--- Lab 2/parametricEstimator1D.m
+++ Lab 2/parametricEstimator1D.m
@@ -1,37 +1,34 @@
-function parametricEstimator1D(input_data)
+function [ans_sigma, ans_mu] = parametricEstimator1D(input_data)
     syms mu sigma_x lambda a b x
-    gauss = @(sigma_x, mu, x) (1/(2*pi*sigma_x^2)^(1/2)) * exp(-0.5*(x-mu)^2/(sigma_x^2));
+    gauss = @(sigma_x, mu, x) (1/(2*pi*sigma_x)^(1/2)) * exp(-0.5*(x-mu)^2/(sigma_x));
     exponential = @(lambda, x) lambda*exp(-lambda*x);
 %     uniform = @(a,b,x) piecewise((a<x)&&(x<b), 1/(b-a) * (b-a)/2, 0);
     uniform = @(a,b,x) (b+a)/2;
 
-    
     log_likelihood_gauss = 0;
     log_likelihood_exponential = 0;
     likelihood_uniform = 1;
     for i = 1:length(input_data)
          log_likelihood_gauss = log_likelihood_gauss + log(gauss(sigma_x, mu, input_data(i)));
-         log_likelihood_exponential = lqog_likelihood_exponential + log(exponential(lambda,input_data(i)));  
+         log_likelihood_exponential = log_likelihood_exponential + log(exponential(lambda,input_data(i)));  
          likelihood_uniform = likelihood_uniform * uniform(a, b, input_data(i));
     end
-%     dGauss_dSigma = diff(log_likelihood_gauss, sigma_x);
-%     dGauss_dMu = diff(log_likelihood_gauss, mu);
-%     ans_mu = double(solve(dGauss_dMu, mu));
-%     ans_sigma = double(solve(subs(dGauss_dSigma,mu, ans_mu), sigma_x));
-%     
+    
+    dGauss_dSigma = diff(log_likelihood_gauss, sigma_x);
+    dGauss_dMu = diff(log_likelihood_gauss, mu);
+    ans_mu = double(solve(dGauss_dMu, mu));
+    ans_sigma = double(solve(subs(dGauss_dSigma,mu, ans_mu), sigma_x));
+     
 %     dExp_dLambda = diff(log_likelihood_exponential, lambda);
 %     ans_lambda = double(solve(dExp_dLambda, lambda));
-    likelihood_uniform = log(likelihood_uniform);
-    d_likelihood_uniform_d_a = diff(likelihood_uniform, a);
-    d_likelihood_uniform_d_b = diff(likelihood_uniform, b);
+%     likelihood_uniform = log(likelihood_uniform);
+%     d_likelihood_uniform_d_a = diff(likelihood_uniform, a);
+%     d_likelihood_uniform_d_b = diff(likelihood_uniform, b);
     
-    a_ans = (solve(d_likelihood_uniform_d_a, a));
-    b_ans = solve(d_likelihood_uniform_d_b, b);
-    
-    a_ans = subs(a_ans(b, b_ans))
+%     a_ans = (solve(d_likelihood_uniform_d_a, a));
+%     b_ans = solve(d_likelihood_uniform_d_b, b);
+%     
+%     a_ans = subs(a_ans(b, b_ans))
 
-    
-    std(input_data)
-    mean(input_data)
 end
 

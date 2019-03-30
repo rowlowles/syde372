7ef27294edb1f5fd961114797e7173b558e179e9
Robert Lowles <rwlowles@edu.uwaterloo.ca>
2019-03-30 18:07:03 -0400

    RL Finished up some parzen work

--- Lab 2/lab2.m
+++ Lab 2/lab2.m
@@ -7,12 +7,21 @@ load('lab2_2');
 a_distribution = gaussian(5,1);
 b_distribution = exponential(1);
 
-ModelEstimation1D(a,a_distribution, "A", 0:0.01:10);
-ModelEstimation1D(b,b_distribution, "B", 0:0.01:10);
+%% Gaussiand and Exponential Distrbutions
+ModelEstimation1D(a,a_distribution, 'A', 0:0.01:10);
+ModelEstimation1D(b,b_distribution, 'B', 0:0.01:10);
+
+%% Parzen Method
+parzenEstimator(a,a_distribution,.1)
+parzenEstimator(a,a_distribution,.4)
+
 
 %%
 % Parametric Estimation - 2D
 parametricEstimation2D(al,bl,cl);
+
+%% Parzen Method - 2D
+
 
 %%
 load('lab2_3');
